eb26d392d2c99d7317e11c2c57545dc8a84d427d
Mario Ramirez <ramsesrago@gmail.com>
2023-10-16 13:17:36 -0600

    First commit

---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" default))
+ '(package-selected-packages '(monokai-theme cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(tool-bar-mode -1)
+
+(load-theme 'monokai t)
+
+;; Standard load
+(require 'no-littering)
+
+;; or with use-package
+(use-package no-littering)
+
+
